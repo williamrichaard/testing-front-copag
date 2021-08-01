6357acbade6509a78eec078e440a21cc60549cae
williamrichaard <wrichaard@hotmail.com>
2021-08-01 20:43:40 -0300

    updated support index

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,3 +18,12 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+Cypress.Server.defaults({
+    delay: 500,
+    force404: false,
+    ignore: (xhr) => {
+      return true;
+    }
+  })
+  
+  Cypress.on('uncaught:exception', (err, runnable) => { return false })
