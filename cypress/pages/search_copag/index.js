ca0a837dee0a16ab51e0e0ba758fab71b10c4c3d
williamrichaard <wrichaard@hotmail.com>
2021-08-01 19:59:40 -0300

    updated index search

--- cypress/pages/search_copag/index.js
+++ cypress/pages/search_copag/index.js
@@ -1,8 +1,14 @@
 import Base from '../../pages/base_page'
+import { Search as src } from './elements'
 
 export class Search extends Base {
 
     static acessar_site(){
         cy.visit('/')
+    }
+
+    static pesquisar_por_produto(texto){
+        cy.get(src.INPUT_PESQUISA).type("digimon");
+        super.clickOnElement(src.BTN_PESQUISA)
     }
 }
