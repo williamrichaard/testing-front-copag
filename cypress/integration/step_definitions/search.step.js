44f43eb25715749fbe2243c30c55e32c66d322e1
williamrichaard <wrichaard@hotmail.com>
2021-08-01 19:13:57 -0300

    added steps search

--- cypress/integration/step_definitions/search.step.js
+++ cypress/integration/step_definitions/search.step.js
@@ -4,3 +4,30 @@ import { Given, When, Then, And, Before } from 'cypress-cucumber-preprocessor/st
 Given(`que esteja na página home da Gopag`, () => {
 	cy.visit('/')
 });
+
+When(`o usuário pesquisa por {string}`, (args1) => {
+	console.log(args1);
+	return true;
+});
+
+Then(`uma imagem de erro sem resultados é mostrada`, () => {
+	return true;
+});
+
+Then(`nenhum produto é listado`, () => {
+	return true;
+});
+
+When(`o usuário pesquisa {string}`, (args1) => {
+	console.log(args1);
+	return true;
+});
+
+Then(`os produtos são listados`, () => {
+	return true;
+});
+
+Then(`os resultados da pesquisa mostram os produtos relacionados a {string}`, (args1) => {
+	console.log(args1);
+	return true;
+});
