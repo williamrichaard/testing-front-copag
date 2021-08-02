c98784f3560f54ea0a1149c811a23ed6c0459dff
williamrichaard <wrichaard@hotmail.com>
2021-08-02 13:53:07 -0300

    updated commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('stepNoImplemented', () => {
+    console.log('O step não foi implementado!');
+    cy.log('O step não foi implementado!')
+})
